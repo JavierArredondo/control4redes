1d67edd142beb4a33309e4f34ae22c034b3e1c7d
JavierArredondo <javier.arredondo.c@usach.cl>
2018-08-15 14:50:35 -0300

    Done

--- cliente.c
+++ cliente.c
@@ -11,21 +11,34 @@
 # include <netinet/in.h>
 # include <netdb.h>
 int main(int argc, char **argv){
+  if(argv[1] == NULL)
+  {
+    printf("Ingrese protocolo TCP o UDP\n");
+    return 1;
+  }
   struct sockaddr_in client; 
   struct hostent* server;
   server = gethostbyname("localhost");
   char buffer[100];
-  int connection = socket(AF_INET, SOCK_DGRAM, 0);
+  int connection;
+  if(strcmp(argv[1], "TCP") == 0)
+    connection = socket(AF_INET, SOCK_STREAM, 0);
+  else if(strcmp(argv[1], "UDP") == 0)
+    connection = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  else
+  {
+    printf("Protocolo no aceptado\n");
+    return 1;
+  }
   int port = 1313;
   bzero((char *)&client, sizeof((char *)&client)); // Atributos de la estructura seteados en 0
   client.sin_family = AF_INET;
   client.sin_port = htons(port);
-
   bcopy((char *)server->h_addr, (char *)&client.sin_addr.s_addr, sizeof(server->h_length));
   int congrat = connect(connection, (struct sockaddr *)&client, sizeof(client));
   if(congrat < 0)
   {
-    printf("Error con el host\n");
+    printf("No se encontro host\n");
     close(connection);
     return 1;
   }
@@ -38,7 +51,7 @@ int main(int argc, char **argv){
     send(connection, messagge, 100, 0); // Envio de hola mundo
     bzero(buffer, 100);              // Seteamos el bufffer de ceros. Rpta
     recv(connection, buffer, 100, 0); // Mensaje de recibido
-    printf("%s", buffer);
+    printf("%s\n", buffer);
   }
   return 0;
 }
--- servidor.c
+++ servidor.c
@@ -11,25 +11,28 @@
 # include <netinet/in.h>
 # include <netdb.h>
 int main(int argc, char **argv){
+   if(argv[1] == NULL)
+  {
+    printf("Ingrese protocolo TCP o UDP\n");
+    return 1;
+  }
   int port = 1313;
   socklen_t longc;
   char buffer[100];
   struct sockaddr_in server;
   struct sockaddr_in client;
   int server_connection;
+  int congrat;
   bzero((char *)&server, sizeof(server));                  // Inicializamos la estructura con ceros contiguos.
   // Inicializamos la estructura con valores utiles para la conexion del server-
   server.sin_family = AF_INET;
   server.sin_port = htons(port);
   server.sin_addr.s_addr = INADDR_ANY;
 
-
-
-
   if(strcmp(argv[1], "TCP") == 0)
   {
     server_connection= socket(AF_INET, SOCK_STREAM, 0); // Socket DGRAM->UDP | STREAM->TCP
-    int congrat = bind(server_connection, (struct sockaddr *)&server, sizeof(server));
+    congrat = bind(server_connection, (struct sockaddr *)&server, sizeof(server));
     if(congrat < 0)
     {
       printf("Error al levantar el servidor.\n");
@@ -52,54 +55,33 @@ int main(int argc, char **argv){
     {
       printf("%s\n", buffer);
       bzero((char *)&buffer, sizeof(buffer));
-      send(client_connection, "Recibido\n", 13, 0);
+      send(client_connection, "Recibido", 13, 0);
     }
   }
   else if(strcmp(argv[1], "UDP") == 0)
   {
     server_connection= socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // Socket DGRAM->UDP | STREAM->TCP
-    int congrat = bind(server_connection, (struct sockaddr *)&server, sizeof(server));
+    congrat = bind(server_connection, (struct sockaddr *)&server, sizeof(server));
     if(congrat < 0)
     {
       printf("Error al levantar el servidor.\n");
       close(server_connection);
       return 1;
     }
-  }
-
-  int congrat = bind(server_connection, (struct sockaddr *)&server, sizeof(server));
-  if(congrat < 0)
-  {
-    printf("Error al levantar el servidor.\n");
-    close(server_connection);
-    return 1;
-  }
-  listen(server_connection, 3);
-  printf("Conexion establecida en el puerto: %d\n", ntohs(server.sin_port));
-  longc = sizeof(client); //  Tamaño de estructura del cliente
-  int client_connection = accept(server_connection, (struct sockaddr*)&client, &longc); // Esperamos la conexion del cliente
-  printf("%i\n", client_connection);
-  /*if(client_connection < 0)
-  {
-    printf("Error del cliente.\n");
-    close(server_connection);
-    return 1;
-  }*
-  printf("Conectado con un cliente: %s:%d\n", inet_ntoa(client.sin_addr),htons(client.sin_port));
-  while(recv(client_connection, buffer, 100, 0) != 0)
-  {
-    printf("%s\n", buffer);
-    bzero((char *)&buffer, sizeof(buffer));
-    send(client_connection, "Recibido\n", 13, 0);
-  }*/
-  while(1)
+    listen(server_connection, 3);
+    printf("Conexion establecida en el puerto: %d\n", ntohs(server.sin_port));
+    longc = sizeof(client); //  Tamaño de estructura del cliente
+    int r;
+    while((r = recvfrom(server_connection, buffer, 100, 0, (struct sockaddr *) &client, &longc)) == 100)
     {
-        int lenClient = sizeof(client);
-        int r = recvfrom(server_connection, buffer, 100, 0, (struct sockaddr *) &client, &lenClient);
-        printf("Received packet from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
         printf("%s\n" , buffer);
-        sendto(server_connection, "Recibido", r, 0, (struct sockaddr*) &client, lenClient);
+        sendto(server_connection, "Recibido", r, 0, (struct sockaddr*) &client, longc);
     }
+  }
+  else
+  {
+    printf("Protocolo no aceptado\n");
+  }
   close(server_connection);
   return 0;
 }
